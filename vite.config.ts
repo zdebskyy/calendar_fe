a56a9987d1e1b7fcbbb6273117ca78cc41998265
Oleksand Zdebskyi <zdebskyi.o@gmail.com>
2026-03-06 15:46:33 +0200

    update config, add toml config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,22 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [
-      react({
-        jsxImportSource: '@emotion/react',
-        babel: {
-          plugins: ['@emotion/babel-plugin'],
-        },
-      }),
-    ],
-    server: {
-      port: 5173,
-      proxy: {
-        '/api': {
-          target: 'https://calendar-be-navy.vercel.app',
-          changeOrigin: true,
-        },
+export default defineConfig({
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+  ],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
       },
     },
-  };
+  },
 });
